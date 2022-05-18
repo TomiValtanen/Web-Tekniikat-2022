4820d7ea58ead0c798ed6a522aaa0571813ef545
TomiValtanen <tomi.valtanen1991@gmail.com>
2022-05-18 22:48:09 +0300

    Kuva lisätty images kansioon + tavoitteet html. Tavoitteet.html tehty modal missä se kuva myös näkyy. Mietteitä blog hommaan lisätty transition max-height.

--- Palautettava työ/tavoitteet.js
+++ Palautettava työ/tavoitteet.js
@@ -15,7 +15,13 @@ function openMietteitä() {
     document.querySelector("div.mietteitä").style.display = "block";
 }
 
-function readMore(site) {
+/*Tälläistä myös olin ajatellut , mutta laitoin sitten tuon toisen kun tahoin transition 
+,että se olisi omasta mielestä hieman smoothimpi eikä töks töks. HTML puolella sitten oli noi
+...(.dots) ja tuo (.more). css puolella oli .more laitettu display none.
+
+*/
+
+/*function readMore(site) {
     let dots = document.querySelector(`.blog-post[data-site="${site}"] .dots`);
     let moreText = document.querySelector(`.blog-post[data-site="${site}"] .more`);
     let btnText = document.querySelector(`.blog-post[data-site="${site}"] .myBtn`);
@@ -32,4 +38,34 @@ function readMore(site) {
         btnText.innerHTML = "Piilota";
         moreText.style.display = "inline";
     }
+}*/
+function readMore(site){
+    let p = document.querySelector(`.blog-post[data-site="${site}"] p:nth-child(3)`);
+    let btnText = document.querySelector(`.blog-post[data-site="${site}"] .myBtn`);
+    if (p.classList.contains('collapsePara')===false) {
+        p.classList.add('collapsePara');
+        btnText.innerHTML = "Piilota";
+      } else {
+        p.classList.remove('collapsePara');
+        btnText.innerHTML = "Lue lisää";
+      }
+}
+
+let modal=document.querySelector(".modal");
+var img = document.querySelector("#wow");
+var modalImg = document.getElementById("img01");
+
+img.addEventListener("click",modalOpen);
+
+function modalOpen(){
+  modal.style.display = "block";
+  modalImg.src = this.src;
+  }
+
+let modalBtn=document.querySelector(".modal button");
+let modalBtnClose=document.querySelector("div.modal-footer button");
+modalBtn.addEventListener("click",closeModal);
+modalBtnClose.addEventListener("click",closeModal);
+function closeModal(){
+    modal.style.display = "none";
 }
